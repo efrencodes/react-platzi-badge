050362b33baab800eff3a7f04de9ec917d6b0bf5
efrenmartinez <developer.emartinez@gmail.com>
2021-07-12 16:52:17 -0500

    :sparkles: Add BadgeList Component

---
+++ src/components/BadgeList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class BadgeList extends React.Component {
+    render() {
+        return (
+            <ul className="list-unstyled">
+                {
+                    this.props.badges.map((badge) => {
+                        return (
+                            <li key={badge.id}>
+                                {badge.firstName} {badge.lastName}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
+export default BadgeList
