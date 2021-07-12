16d06de260e82d4f17b866fe7419a25bda4c7633
efrenmartinez <developer.emartinez@gmail.com>
2021-07-12 15:48:49 -0500

    :lipstick: Update in style in Badge Component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -13,11 +13,11 @@ class Badge extends React.Component {
             </div>
             <div className="badge_section-name">
                 <img className="badge_avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
-                <h1>{this.props.firstName}<br />{this.props.lastName}</h1>
+                <h1>{this.props.firstName || 'Efren'}<br />{this.props.lastName || 'Martinez'}</h1>
             </div>
             <div className="badge_section-info">
-                <h3>{this.props.job || 'Programador'}</h3>
-                <div>@{this.props.twitter || 'twitter'}</div>
+                <h3>{this.props.job || 'Desarrollador Front End'}</h3>
+                <div>@{this.props.twitter || 'efrenmartinezmx'}</div>
             </div>
             <div className="badge_footer">
                 #platziconf
