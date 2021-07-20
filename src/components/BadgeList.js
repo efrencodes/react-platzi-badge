1e772b53cb11feade4469cf37470fbea82fb22da
efrenmartinez <developer.emartinez@gmail.com>
2021-07-19 22:19:20 -0500

    :sparkles: Add request list

--- src/components/BadgeList.js
+++ src/components/BadgeList.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import '../styles/BadgeList.css'
 import BadgesListItem from './BadgeListItem'
+import { Link } from 'react-router-dom'
 
 class BadgeList extends React.Component {
     render() {
+        if (this.props.badges.length === 0) {
+            return (
+                <div className="BadgeList">
+                    <ul className="list-unstyled">
+                        <h4>NO FOUND DATA</h4>
+                        <Link to="/new" className="btn btn-primary">new Badge</Link>
+                    </ul>
+                </div>
+            )
+        }
         return (
             <div className="BadgeList">
                 <ul className="list-unstyled">
