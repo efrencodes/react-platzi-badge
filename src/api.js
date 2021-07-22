903cce8381b31066bbbca19575b1cf6ff1426aac
efrenmartinez <developer.emartinez@gmail.com>
2021-07-22 11:44:31 -0500

    :sparkles: Update const BASE_URL

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://react-platzi-badge.vercel.app/3001' // 'http://localhost:3001';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
